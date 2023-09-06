91d2a471f11e829fc364d14bf0c01cd41f0e0a61
Derick Zhen <dzhen@purdue.edu>
2023-09-06 12:45:32 -0400

    lab3

--- cgt215_lab3.cpp
+++ cgt215_lab3.cpp
@@ -3,18 +3,87 @@
 
 #include <iostream>
 
-int main()
+using namespace std;
+
+void printMenu(int& choice) 
 {
-    std::cout << "Hello World!\n";
+	cout << "Welcome to my program, please select an operation to perform:" << endl;
+	cout << "1. Addition" << endl;
+	cout << "2. Subtraction" << endl;
+	cout << "3. Multiplication" << endl;
+	cout << "4. Division" << endl;
+
+	cout << "\nYour Selection: ";
+	cin >> choice;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void getChoices(float& A, float& B) 
+{
+	cout << "Please enter the first value:";
+	cin >> A;
+	cout << "Please enter the second value:";
+	cin >> B;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void firstChoice(float A, float B) 
+{
+	cout << "This is proving the first choice ran" << endl;
+}
+
+void secondChoice(float A, float B)
+{
+	cout << "This is proving the second choice ran" << endl;
+}
+
+int main() {
+	int choice;
+	float A;
+	float B;
+	printMenu(choice);
+	cout << endl;
+	getChoices(A, B);
+	
+
+	if (choice == 1) 
+	{
+		cout << endl;
+		cout << A << "+" << B << "=" << A + B;
+	}
+
+	if (choice == 2) 
+	{
+
+		cout << endl;
+		cout << A << "-" << B << "=" << A - B;
+	}
+
+	if (choice == 3) 
+	{
+		cout << endl;
+		cout << A << "*" << B << "=" << A * B;
+	}
+
+	if (choice == 4) 
+	{
+		cout << endl;
+		cout << A << "/" << B << "=" << A / B;
+	}
+
+	if (choice > 4)
+	{
+		cout << endl;
+		cout << "nope i give up" << endl;
+		return 0;
+	}
+
+	if (choice < 0)
+	{
+		cout << endl;
+		cout << "nope i give up" << endl;
+		return 0;
+	}
+
+
+
+	return 0;
+}
